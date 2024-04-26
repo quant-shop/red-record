a73c38e6193513c004bedab2fc5c4ac089066c54
professornaite <professornaite@gmail.com>
2024-04-26 04:32:07 -0400

    plots

--- code/record-1983.R
+++ code/record-1983.R
@@ -3,6 +3,10 @@ library(dplyr)
 library(ggplot2)
 library(stringr)
 library(magrittr)
+install.packages("xts")          # To make the convertion data-frame / xts format
+library(xts)
+install.packages("dygraphs")
+library(dygraphs)
 
 ## load date; cite from caitlin p (2015)
 record1893 <- read.csv("/Users/nathanalexander/Dropbox/Projects/lynching/IdaBWellsProject/RedRecord/redrecord1893.csv")
@@ -27,14 +31,20 @@ record1893 %>%
   mutate(date = as.Date(Date, format = "%b.%d,%Y")) -> record1893
 
 str(record1893)
-
+glimpse(record1893)
 
 ggplot(record1893, aes(x=date, y=count)) +
   geom_line( color="steelblue") + 
   geom_point() +
-  xlab("") +
-  theme(axis.text.x=element_text(angle=60, hjust=1)) +
+  xlab("Date") + 
   ylim(1,7)
-  
 
-glimpse(record1893)
+
+don <- xts(x = record1893$count, order.by = record1893$date)
+p <- dygraph(don) %>%
+  dyOptions(labelsUTC = TRUE, fillGraph=TRUE, fillAlpha=0.1, drawGrid = FALSE, colors="#D8AE5A") %>%
+  dyRangeSelector() %>%
+  dyCrosshair(direction = "vertical") %>%
+  dyHighlight(highlightCircleSize = 5, highlightSeriesBackgroundAlpha = 0.2, hideOnMouseOut = FALSE)  %>%
+  dyRoller(rollPeriod = 1)
+p
